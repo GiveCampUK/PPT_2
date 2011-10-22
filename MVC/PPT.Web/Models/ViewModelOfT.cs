b0a14af391a032053139b4976c688ee1fb27b1ae
Jason Mitchell <jasonmitchell@live.co.uk>
2011-10-22 23:14:53 +0100

    fixed edit form

---
+++ MVC/PPT.Web/Models/ViewModelOfT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPT.Web.Models
+{
+    public class ViewModelOf<T>
+    {
+        public T Model { get; set; }
+
+        public ViewModelOf(T model)
+        {
+            Model = model;
+        }
+    }
+}
