5634ad66b31632f3ca21dc501a56c3fc9f3395b5
Jon David <jd.givecamp2011@gmail.com>
2011-10-22 21:51:13 +0100

    DB Script Updates

--- Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Type.cs
+++ Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Type.cs
@@ -10,103 +10,107 @@ namespace Ppt.DataMigration.Services.Prisoner
 {
     public class Lookup_Type : AbstractTableMigrationService
     {
-        public string AccessTableName { get; set; }
-
-        public Lookup_Type()
-        {
-            AccessTableName = "Lookup_Type";
-        }
-             public string AccessTableName { get; set; }
-
-             public Lookup_Type()
-        {
-            AccessTableName= "Lookup_Type";
-        }
         public override void MigrateTable()
         {
+            throw new NotImplementedException();
+        }
+        //public string AccessTableName { get; set; }
 
-            try
-            {
-                SQLConnection.Open();
-                AccessConnection.Open();
-                //Get Access Data
+        //public Lookup_Type()
+        //{
+        //    AccessTableName = "Lookup_Type";
+        //}
+        //     public string AccessTableName { get; set; }
 
-                OleDbCommand oleCmd = AccessConnection.CreateCommand();
-                oleCmd.CommandText = "SELECT * FROM " + AccessTableName;
+        //     public Lookup_Type()
+        //{
+        //    AccessTableName= "Lookup_Type";
+        //}
+        //public override void MigrateTable()
+        //{
 
-                //get current records in SQL
-                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM COUNTRY", SQLConnection);
+        //    try
+        //    {
+        //        SQLConnection.Open();
+        //        AccessConnection.Open();
+        //        //Get Access Data
 
-                DataSet sqlCountry = new DataSet("Country");
-                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "COUNTRY");
-                sqlAdapter.Fill(sqlCountry);
-                DataTable dt = sqlCountry.Tables["COUNTRY"];
+        //        OleDbCommand oleCmd = AccessConnection.CreateCommand();
+        //        oleCmd.CommandText = "SELECT * FROM " + AccessTableName;
 
-                var reader = oleCmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    var results = dt.Select("Name = '{0}'".Formatted(reader["COUNTRY"]));
-                    if (results.Length == 0)
-                    {
-                        var newRow = dt.NewRow();
-                        newRow["Name"] = reader["COUNTRY"];
-                        dt.Rows.Add(newRow);
-                    }
-                }
-                reader.Close();
-                dt.AcceptChanges();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                AccessConnection.Close();
-                SQLConnection.Close();//should we open and close for each database?
-            }
+        //        //get current records in SQL
+        //        SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM COUNTRY", SQLConnection);
+
+        //        DataSet sqlCountry = new DataSet("Country");
+        //        sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "COUNTRY");
+        //        sqlAdapter.Fill(sqlCountry);
+        //        DataTable dt = sqlCountry.Tables["COUNTRY"];
+
+        //        var reader = oleCmd.ExecuteReader();
+        //        while (reader.Read())
+        //        {
+        //            var results = dt.Select("Name = '{0}'".Formatted(reader["COUNTRY"]));
+        //            if (results.Length == 0)
+        //            {
+        //                var newRow = dt.NewRow();
+        //                newRow["Name"] = reader["COUNTRY"];
+        //                dt.Rows.Add(newRow);
+        //            }
+        //        }
+        //        reader.Close();
+        //        dt.AcceptChanges();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //    finally
+        //    {
+        //        AccessConnection.Close();
+        //        SQLConnection.Close();//should we open and close for each database?
+        //    }
 
 
 
-                //get current records in SQL
-                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM PersonType", SQLConnection);
+        //        //get current records in SQL
+        //        SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM PersonType", SQLConnection);
 
-                SqlCommandBuilder oOrderDetailsCmdBuilder = new
-                SqlCommandBuilder(sqlAdapter);
+        //        SqlCommandBuilder oOrderDetailsCmdBuilder = new
+        //        SqlCommandBuilder(sqlAdapter);
 
-                DataSet sqlCountry = new DataSet("PersonType");
-                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "PersonType");
-                sqlAdapter.Fill(sqlCountry);
-                DataTable dt = sqlCountry.Tables["PersonType"];
+        //        DataSet sqlCountry = new DataSet("PersonType");
+        //        sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "PersonType");
+        //        sqlAdapter.Fill(sqlCountry);
+        //        DataTable dt = sqlCountry.Tables["PersonType"];
 
 
                 
-                var reader = oleCmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    var results = dt.Select("Name = '{0}'".Formatted(reader["CODE"]));
-                    if (results.Length == 0)
-                    {
-                        var newRow = dt.NewRow();
-                        newRow["Name"] = reader["DESC"];
-                        newRow["ShortCode"] = reader["CODE"];
-                        dt.Rows.Add(newRow);
-                    }
-                }
-                reader.Close();
-                sqlAdapter.Update(dt);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                AccessConnection.Close();
-                SQLConnection.Close();//should we open and close for each database?
-            }
+        //        var reader = oleCmd.ExecuteReader();
+        //        while (reader.Read())
+        //        {
+        //            var results = dt.Select("Name = '{0}'".Formatted(reader["CODE"]));
+        //            if (results.Length == 0)
+        //            {
+        //                var newRow = dt.NewRow();
+        //                newRow["Name"] = reader["DESC"];
+        //                newRow["ShortCode"] = reader["CODE"];
+        //                dt.Rows.Add(newRow);
+        //            }
+        //        }
+        //        reader.Close();
+        //        sqlAdapter.Update(dt);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //    finally
+        //    {
+        //        AccessConnection.Close();
+        //        SQLConnection.Close();//should we open and close for each database?
+        //    }
 
 
-        }
+        //}
     }
 }
