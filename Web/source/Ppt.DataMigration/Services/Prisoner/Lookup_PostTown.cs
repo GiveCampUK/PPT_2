138fc0834f21e237e5d1ebc9cb73642839d60f52
Jon David <jd.givecamp2011@gmail.com>
2011-10-22 19:26:15 +0100

    More

--- Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_PostTown.cs
+++ Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_PostTown.cs
@@ -10,9 +10,56 @@ namespace Ppt.DataMigration.Services.Prisoner
 {
     public class Lookup_PostTown : AbstractTableMigrationService
     {
+        public string AccessTableName { get; set; }
+
+        public Lookup_PostTown()
+        {
+            AccessTableName= "LOOKUP_POSTTOWN";
+        }
+
         public override void MigrateTable()
         {
-            
+            try
+            {
+                SQLConnection.Open();
+                AccessConnection.Open();
+
+                //Get Access Data
+                OleDbCommand oleCmd = AccessConnection.CreateCommand();
+                oleCmd.CommandText = "SELECT * FROM " + AccessTableName;
+
+                //get current records in SQL
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM Town", SQLConnection);
+               
+                DataSet sqlCountry = new DataSet("Town");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "Town");
+                sqlAdapter.Fill(sqlCountry);
+                DataTable dt = sqlCountry.Tables["Town"];
+
+                var reader = oleCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var results = dt.Select("Name = '{0}'".Formatted(reader["Town"]));
+                    if (results.Length == 0)
+                    {
+                        var newRow = dt.NewRow();
+                        newRow["Name"] = reader["Town"];
+                        dt.Rows.Add(newRow);
+                    }
+                }
+
+                reader.Close();
+                dt.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                AccessConnection.Close();
+                SQLConnection.Close();//should we open and close for each database?
+            }
         }
     }
 }
--- Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Type.cs
+++ Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Type.cs
@@ -10,9 +10,56 @@ namespace Ppt.DataMigration.Services.Prisoner
 {
     public class Lookup_Type : AbstractTableMigrationService
     {
+        public string AccessTableName { get; set; }
+
+        public Lookup_Type()
+        {
+            AccessTableName = "Lookup_Type";
+        }
         public override void MigrateTable()
         {
-            
+            try
+            {
+                SQLConnection.Open();
+                AccessConnection.Open();
+                //Get Access Data
+
+                OleDbCommand oleCmd = AccessConnection.CreateCommand();
+                oleCmd.CommandText = "SELECT * FROM " + AccessTableName;
+
+                //get current records in SQL
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM COUNTRY", SQLConnection);
+
+                DataSet sqlCountry = new DataSet("Country");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "COUNTRY");
+                sqlAdapter.Fill(sqlCountry);
+                DataTable dt = sqlCountry.Tables["COUNTRY"];
+
+                var reader = oleCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var results = dt.Select("Name = '{0}'".Formatted(reader["COUNTRY"]));
+                    if (results.Length == 0)
+                    {
+                        var newRow = dt.NewRow();
+                        newRow["Name"] = reader["COUNTRY"];
+                        dt.Rows.Add(newRow);
+                    }
+                }
+                reader.Close();
+                dt.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                AccessConnection.Close();
+                SQLConnection.Close();//should we open and close for each database?
+            }
+
+
         }
     }
 }
