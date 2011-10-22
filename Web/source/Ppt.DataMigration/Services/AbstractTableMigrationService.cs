b29d4426abaa270040741ac840a17233a309bf81
Michael Edwards <mikeedwards83@googlemail.com>
2011-10-22 20:19:24 +0100

    migration changes

--- Web/source/Ppt.DataMigration/Services/AbstractTableMigrationService.cs
+++ Web/source/Ppt.DataMigration/Services/AbstractTableMigrationService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Castle.Core.Logging;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using System.Data;
 
 namespace Ppt.DataMigration.Services
 {
@@ -34,6 +35,77 @@ namespace Ppt.DataMigration.Services
 
 
         public abstract void MigrateTable();
-       
+
+
+        DataTable _towns = null;
+
+        public object GetTownsFromSql(string townName)
+        {
+            if (_towns == null)
+            {
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM Town", SQLConnection);
+
+                SqlCommandBuilder oOrderDetailsCmdBuilder = new
+                SqlCommandBuilder(sqlAdapter);
+
+                DataSet sqlCountry = new DataSet("Town");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "Town");
+                sqlAdapter.Fill(sqlCountry);
+                _towns = sqlCountry.Tables["Town"];
+            }
+
+            var result = _towns.Select("Name = '{0}'".Formatted(townName));
+            if(result.Length == 0) return null;
+            else return result[0]["Id"]; 
+        }
+
+        DataTable _countrys = null;
+
+        public object GetCountryFromSql(string country)
+        {
+            if (_countrys == null)
+            {
+
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM Country", SQLConnection);
+
+                SqlCommandBuilder oOrderDetailsCmdBuilder = new
+                SqlCommandBuilder(sqlAdapter);
+
+                DataSet sqlCountry = new DataSet("Country");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "Country");
+                sqlAdapter.Fill(sqlCountry);
+                _countrys = sqlCountry.Tables["Country"];
+            }
+
+             var result = _towns.Select("Name = '{0}'".Formatted(country));
+            if(result.Length == 0) return null;
+            else return result[0]["Id"]; 
+
+        }
+
+        DataTable _prisonSex = null;
+
+        public object GetPrisonSexFromSql(string prisonSex)
+        {
+            if (_countrys == null)
+            {
+
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM PrisonSex", SQLConnection);
+
+                SqlCommandBuilder oOrderDetailsCmdBuilder = new
+                SqlCommandBuilder(sqlAdapter);
+
+                DataSet sqlCountry = new DataSet("PrisonSex");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "PrisonSex");
+                sqlAdapter.Fill(sqlCountry);
+                _prisonSex = sqlCountry.Tables["PrisonSex"];
+            }
+
+            var result = _towns.Select("Name = '{0}'".Formatted(prisonSex));
+            if(result.Length == 0) return null;
+            else return result[0]["Id"]; 
+
+        }
+
     }
 }
--- Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Prisons.cs
+++ Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Prisons.cs
@@ -10,9 +10,105 @@ namespace Ppt.DataMigration.Services.Prisoner
 {
     public class Lookup_Prisons : AbstractTableMigrationService
     {
+       
+             public string AccessTableName { get; set; }
+
+             public Lookup_Prisons()
+        {
+            AccessTableName= "Lookup_Prisons";
+        }
         public override void MigrateTable()
         {
-            
+
+            try
+            {
+                SQLConnection.Open();
+                AccessConnection.Open();
+                
+                //turn of ID column OFF
+
+                SqlCommand identOff = new SqlCommand("SET IDENTITY_INSERT Prison OFF", SQLConnection);
+                identOff.ExecuteScalar();
+                
+                //Get Access Data
+
+                OleDbCommand oleCmd = AccessConnection.CreateCommand();
+                oleCmd.CommandText = "SELECT * FROM " + AccessTableName;
+
+
+                //get current records in SQL
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM PRISON", SQLConnection);
+
+                SqlCommandBuilder oOrderDetailsCmdBuilder = new
+                SqlCommandBuilder(sqlAdapter);
+
+                DataSet sqlCountry = new DataSet("Prison");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "Prison");
+                sqlAdapter.Fill(sqlCountry);
+                DataTable dt = sqlCountry.Tables["Prison"];
+
+
+                
+                var reader = oleCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var results = dt.Select("Name = '{0}'".Formatted(reader["Prison"]));
+                    if (results.Length == 0)
+                    {
+                        var newRow = dt.NewRow();
+                        newRow["Id"] = reader["PRISON ID"];
+                        newRow["Name"] = reader["PRISON"];
+                        newRow["MailName"] = reader["MailName"];
+                        newRow["Address1"] = reader["Add1"];
+                        newRow["Address2"] = reader["Add2"];
+                        newRow["Address3"] = reader["Add3"];
+                        newRow["Town"] = reader["Town"];
+                        newRow["County"] = reader["County"];
+                        newRow["Postcode"] = reader["Postcode"];
+                        newRow["Country"] = GetCountryFromSql(reader["County"] as string);
+                        newRow["Sex"] = GetPrisonSexFromSql(reader["Sex"] as string);
+                        newRow["MalePrisonersHeld"] = reader["MalePrisonersHeld"];
+                        newRow["FemalePrisonersHeld"] = reader["FemalePrisonersHeld"];
+                        newRow["JuvenilePrisonersHeld"] = reader["JuvenilePrisonersHeld"];
+                        newRow["AdultPrisonersHeld"] = reader["AdultPrisonersHeld"];
+                        newRow["Cat"] = reader["Cat"];
+                        newRow["Ptype"] = reader["Ptype"];
+                        newRow["VolunteersNotes"] = reader["Volunteers Notes"];
+                        newRow["Notes"] = reader["Notes"];
+                        newRow["Governor"] = reader["Governor"];
+                        newRow["Telephone"] = reader["Telephone"];
+                        newRow["Tag"] = reader["Tag"];
+                        newRow["NlAddressOrder"] = reader["Nl Address Order"];
+                        newRow["InclInNlAddressList"] = reader["Incl In Nl Address List"];
+                        newRow["DesignationNewsletter"] = reader["Designation Newsletter"];
+                        newRow["Location"] = reader["Location"];
+                        newRow["ContactsForNewsletter"] = reader["ContactsForNewsletter"];
+                        newRow["ContactPosition"] = reader["ContactPosition"];
+                        newRow["ContactPhone"] = reader["ContactPhone"];
+                        newRow["ContactFax"] = reader["ContactFax"];
+                        dt.Rows.Add(newRow);
+                    }
+                }
+                reader.Close();
+                sqlAdapter.Update(dt);
+
+
+                 //turn of ID column ON
+
+                SqlCommand identOn = new SqlCommand("SET IDENTITY_INSERT Prison ON", SQLConnection);
+                identOn.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                AccessConnection.Close();
+                SQLConnection.Close();//should we open and close for each database?
+            }
+
+
         }
     }
 }
