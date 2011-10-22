c582bfdd7549408710ef12d2581b6d8b3918fa8a
Michael Edwards <mikeedwards83@googlemail.com>
2011-10-22 19:37:13 +0100

    We built this city, we build this city on rock and roll!!

--- Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Type.cs
+++ Web/source/Ppt.DataMigration/Services/Prisoner/Lookup_Type.cs
@@ -10,9 +10,64 @@ namespace Ppt.DataMigration.Services.Prisoner
 {
     public class Lookup_Type : AbstractTableMigrationService
     {
+             public string AccessTableName { get; set; }
+
+             public Lookup_Type()
+        {
+            AccessTableName= "Lookup_Type";
+        }
         public override void MigrateTable()
         {
-            
+
+            try
+            {
+                SQLConnection.Open();
+                AccessConnection.Open();
+                //Get Access Data
+
+                OleDbCommand oleCmd = AccessConnection.CreateCommand();
+                oleCmd.CommandText = "SELECT * FROM " + AccessTableName;
+
+
+                //get current records in SQL
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM PersonType", SQLConnection);
+
+                SqlCommandBuilder oOrderDetailsCmdBuilder = new
+                SqlCommandBuilder(sqlAdapter);
+
+                DataSet sqlCountry = new DataSet("PersonType");
+                sqlAdapter.FillSchema(sqlCountry, SchemaType.Source, "PersonType");
+                sqlAdapter.Fill(sqlCountry);
+                DataTable dt = sqlCountry.Tables["PersonType"];
+
+
+                
+                var reader = oleCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var results = dt.Select("Name = '{0}'".Formatted(reader["CODE"]));
+                    if (results.Length == 0)
+                    {
+                        var newRow = dt.NewRow();
+                        newRow["Name"] = reader["DESC"];
+                        newRow["ShortCode"] = reader["CODE"];
+                        dt.Rows.Add(newRow);
+                    }
+                }
+                reader.Close();
+                sqlAdapter.Update(dt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                AccessConnection.Close();
+                SQLConnection.Close();//should we open and close for each database?
+            }
+
+
         }
     }
 }
