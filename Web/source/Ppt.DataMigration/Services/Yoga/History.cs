c86a1219f66a51e1b91a175e3df35393326cc06c
Jon David <jd.givecamp2011@gmail.com>
2011-10-22 23:31:31 +0100

    More data conversion classes

--- Web/source/Ppt.DataMigration/Services/Yoga/History.cs
+++ Web/source/Ppt.DataMigration/Services/Yoga/History.cs
@@ -7,9 +7,47 @@ namespace Ppt.DataMigration.Services.Yoga
 {
     public class History : AbstractTableMigrationService
     {
+        public History()
+        {
+            AccessTableName = "History";
+            NewTableName = "History";
+        }
         public override void MigrateTable()
         {
-            throw new NotImplementedException();
+            try
+            {
+                SQLConnection.Open();
+                AccessConnection.Open();
+
+                //Get Access Data
+                var oleCmd = GetSelectAllCommand();
+                var adapter = GetSqlAdapter();
+                var dataSet = GetAndFillDataSet(adapter);
+                var dt = GetDataTable(dataSet);
+
+                var reader = oleCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var results = dt.Select("ID = '{0}'".Formatted(reader["Id"]));
+                    if (results.Length == 0)
+                    {
+                        var newRow = dt.NewRow();
+                        newRow["Id"] = reader["Id"];
+                        dt.Rows.Add(newRow);
+                    }
+                }
+                reader.Close();
+                adapter.Update(dt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                AccessConnection.Close();
+                SQLConnection.Close();//should we open and close for each database?
+            }
         }
     }
 }
