52de16e29a93f9a1eb133f8efbd87af5469e950f
Adam Christie <fractos@gmail.com>
2011-10-23 05:08:03 +0100

    Things fall apart, the center cannot hold Mere anarchy is loosed upon the world. The blood-dimmed tide is loosed And everywhere, the ceremony of innocence is drowned. And what rough beast, its hour come round at last Slouches towards Bethlehem to be born?

---
+++ thing/Application1/Application1/Client/UserCode/CreateNewContact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewContact
+    {
+        partial void CreateNewContact_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.ContactProperty = new Contact();
+        }
+
+        partial void CreateNewContact_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.ContactProperty);
+        }
+    }
+}
